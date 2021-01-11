9ccbb67f9b3d816678ed717602197cd87d2c73fb
Zane-dev16 <imf.999kg@gmail.com>
2021-01-11 11:33:41 +0000

    Design navbar

--- script/script.js
+++ script/script.js
@@ -7,11 +7,32 @@ function solve() {
         cells = row.cells
         x = -1
         for (cell of cells) {
-            deduction(cell)
+            i = 1
         }
     }
+    console.log(window.scrollY)
 }
 
-let x= [1, 2, 3, 4]
-
-
+document.addEventListener("scroll", (e) => {
+    if (window.scrollY > 25) {
+        window.requestAnimationFrame(() => {
+            let header = document.getElementsByTagName('header')[0]
+            let black = document.createAttribute('class')
+            black.value = "blacken"
+            header.setAttributeNode(black)
+            let slider = document.getElementById('slider')
+            console.log(slider)
+            let slide = document.createAttribute('class')
+            slide.value = 'top_slider slide'
+            slider.setAttributeNode(slide)
+        })
+    } else {
+        window.requestAnimationFrame(() => {
+            let header = document.getElementsByTagName('header')[0]
+            header.removeAttribute('class')
+            let slide = document.createAttribute('class')
+            slide.value = 'top_slider'
+            slider.setAttributeNode(slide)
+        })
+    }
+})
